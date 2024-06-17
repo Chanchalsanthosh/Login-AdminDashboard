ec70d702cb92bd779e1101b845c5d22485f27ed4
Chanchal <114665425+Chanchalsanthosh@users.noreply.github.com>
2024-06-18 03:00:06 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -33,17 +33,20 @@ import React, { useContext } from 'react';
 import './App.css';
 import "./styles/login.css";
 import Login from './login';
-import Home from './pages/Home';
-import Announcement from './pages/Announcement';
-import Gallery from "./pages/Gallery";
+import Home from './pages/AdHome';
+import Announcement from './pages/AdAnnouncement';
+import Gallery from "./pages/AdGallery";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthProvider';
 import AdBlogs from './pages/AdBlogs';
 import AdCompanies from './pages/AdCompanies';
-import AdEvents from './pages/AdEvents';
+import AdEvents from './pages/EventAdminSa (1)';
 import AdStartup from './pages/AdStartup';
 import Admentor from './pages/Admentor';
 import Logout from './logout';
+import Admin_Incubatees from './pages/Admin_Incubatees';
+import AdSubscribers from './pages/AdSubscribers';
+import Admin_Blogs from './pages/Admin_Blogs';
 
 
 function App() {
@@ -58,10 +61,12 @@ function App() {
           <Route path="/adannounce" element={token ? <Announcement/> : <Navigate to="/"/>} />
           <Route path="/adgallery" element={token ? <Gallery/> : <Navigate to="/"/>} />
           <Route path="/adcompanies" element={token ? <AdCompanies/> : <Navigate to="/"/>} />
-          <Route path="/adblogs" element={token ? <AdBlogs/> : <Navigate to="/"/>} />
+          <Route path="/adblogs" element={token ? <Admin_Blogs/> : <Navigate to="/"/>} />
           <Route path="/adevents" element={token ? <AdEvents/> : <Navigate to="/"/>} />
           <Route path="/adstartup" element={token ? <AdStartup/> : <Navigate to="/"/>} />
           <Route path="/admentor" element={token ? <Admentor/> : <Navigate to="/"/>} />
+          <Route path="/adincubate" element={token ? <Admin_Incubatees/> : <Navigate to="/"/>} />
+          <Route path="/adsubcribers" element={token ? <AdSubscribers/> : <Navigate to="/"/>} />
           <Route path="/logout" element={token ? <Logout/> : <Navigate to="/"/>} />
           {token && (
             <>
@@ -72,6 +77,8 @@ function App() {
               <Route path="/adevents" element={<AdEvents />} />
               <Route path="/adstartup" element={<AdStartup />} />
               <Route path="/admentor" element={<Admentor />} />
+              <Route path="/adincubate" element={<Admin_Incubatees />} />
+              <Route path="/adsubcribers" element={<AdSubscribers />} />
               <Route path="/logout" element={<Logout />} />
               
             </>
